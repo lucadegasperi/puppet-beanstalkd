090ea499108a7a72b73436feeb028696e29a5bb3
Luca Degasperi <luca@lucadegasperi.com>
2013-10-26 11:03:02 +0200

    adding beanstalks service at startup

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,10 @@ class beanstalkd {
   package { 'beanstalkd':
     ensure => present,
   }
+
+  service { 'beanstalkd':
+    ensure => 'running',
+    require => Package['beanstalkd'],
+    start => '/usr/bin/beanstalkd'
+  }
 }
